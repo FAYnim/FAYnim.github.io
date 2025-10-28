aaba473bd752ff55d9f6d2e4c00e38a2616f83f1
FAYnim <yuana6898@gmail.com>
2025-10-28 07:03:25 -0700

    Redesign UI proyek

--- script.js
+++ script.js
@@ -31,68 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
     shape.style.animationDuration = `${animationDuration}s`;
   });
 
-  // --- Logika Carousel Proyek ---
-  const track = document.querySelector('.project-track'); // Ambil semua data proyek
-  if (track) { // Cek apakah elemen track ada
-    const slides = Array.from(track.children); // Buat array dari semua slide proyek.
-    const nextButton = document.querySelector('.next-btn');
-    const prevButton = document.querySelector('.prev-btn');
-    const dotsNav = document.querySelector('.carousel-nav-dots');
-    const slideWidth = slides[0].getBoundingClientRect().width; // Dapatkan lebar satu slide.
-
-    let currentIndex = 0; // index slide yang aktif
-
-    // Buat dots navigasi berdasarkan jumlah slide
-    slides.forEach((slide, index) => {
-      const dot = document.createElement('button'); // Buat elemen dot
-      dot.classList.add('dot'); // Tambahkan class dot
-      if (index === 0) dot.classList.add('active'); // Jadikan dot pertama aktif
-      dotsNav.appendChild(dot); // Tambahkan dot ke dalam navigasi
-    });
-
-    const dots = Array.from(dotsNav.children); // Buat array dari semua elemen dot. Sama kayak slides
-
-    // Fungsi untuk memperbarui dot yang aktif
-    const updateDots = (currentDot, targetDot) => {
-      currentDot.classList.remove('active'); // Hapus class active dari dot lama.
-      targetDot.classList.add('active'); // Tambahkan class active ke dot baru.
-    };
-
-    // Fungsi untuk memindahkan slide
-    const moveToSlide = (targetIndex) => {
-      track.style.transform = 'translateX(-' + slideWidth * targetIndex + 'px)'; // Geser track ke posisi slide tujuan.
-      // Rumus untuk geser slide
-      // translateX(- lebarSlide * indexTujuan)
-
-      const currentDot = dots[currentIndex]; // Dot yang aktif
-      const targetDot = dots[targetIndex]; // Dot tujuan
-      updateDots(currentDot, targetDot); // Update status aktif pada dot.
-      currentIndex = targetIndex; // Update indeks slide yang aktif.
-    };
-
-    // Tombol next
-    nextButton.addEventListener('click', () => {
-      const nextIndex = (currentIndex + 1) % slides.length; // Hitung indeks slide berikutnya (looping).
-      // rumus (indeks sekarang + 1) mod jumlah slide
-      moveToSlide(nextIndex); // Pindahkan ke slide berikutnya.
-    });
-
-    // Tombol prev
-    prevButton.addEventListener('click', () => {
-      const prevIndex = (currentIndex - 1 + slides.length) % slides.length; // Hitung indeks slide sebelumnya (looping).
-      // rumus (indeks sekarang - 1 + jumlah slide) mod jumlah slide
-      moveToSlide(prevIndex); // Pindahkan ke slide sebelumnya.
-    });
-
-    // Tombol navigasi lewat dot
-    dotsNav.addEventListener('click', e => {
-      const targetDot = e.target.closest('button.dot'); // Cari elemen dot yang diklik.
-      if (!targetDot) return; // Jika bukan dot yang diklik, abaikan.
-      const targetIndex = dots.findIndex(dot => dot === targetDot); // Cari indeks dari dot yang diklik
-      moveToSlide(targetIndex);
-    });
-  }
-
 	// --- TOGGLE SIDEBAR MOBILE ---
 	const menuToggle = document.getElementById('menu-toggle');
 	const nav = document.querySelector('header nav');
